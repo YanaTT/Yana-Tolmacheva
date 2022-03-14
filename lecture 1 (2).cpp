da245c8861b626d0a9e2bfb61095a605a8c4e684
YanaTT <100983501+YanaTT@users.noreply.github.com>
2022-03-15 01:21:00 +0800

    Create lecture 1 (2).cpp

---
+++ lecture 1 (2).cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+int a=100;
+printf("%d\n ", a);
+float b=35.8;
+printf("%f\n ", b);
+char c='4';
+printf("%d\n ", c);
+double d=-345.63;
+printf("%f\n ", d);
+int e=a/46;
+printf("%d\n ", e);
+double f=d*9;
+printf("%f\n ", f);
+float g=b+89;
+printf("%f\n ", g);
+int h=a%3;
+printf("%d\n ", h);
+return 0;
+}
