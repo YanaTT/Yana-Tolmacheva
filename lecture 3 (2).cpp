ca225e330683d9a18e5d31d059d7db4765f44d5f
YanaTT <100983501+YanaTT@users.noreply.github.com>
2022-04-25 23:16:28 +0800

    Update lecture 3 (2).cpp

--- lecture 3 (2).cpp
+++ lecture 3 (2).cpp
@@ -3,12 +3,11 @@
 int main ()
 {
 int f;
-int n = 1;
 FILE * output;
 output = fopen ("output", "r");
 for (int i = 1; i <= 8; i++){
 fscanf (output, "%d\n", &f);
-printf ("%d.%d\n", n, f);
+printf ("%d.%d\n", i, f);
 
 }
 fclose (output);
