3924aaccf4117fc28c1c2d81932f4eb625f3c5a1
YanaTT <100983501+YanaTT@users.noreply.github.com>
2022-05-18 21:41:46 +0800

    Create lecture 6 (2).cpp

---
+++ lecture 6 (2).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void print (int f); 
+void print (float f);
+void print (char f);
+void print (int f, int c);
+
+int main()
+{
+print (14);
+print (14528764);
+print ('f');
+print (52,589);
+
+return 0;
+}
+
+void print (int f){
+cout << f << "\n";
+}
+void print (float f){
+cout << f << "\n";
+}
+void print (char f){
+cout << f << "\n";
+}
+void print (int f, int c){
+cout << f <<", " << c << "\n";
+}
